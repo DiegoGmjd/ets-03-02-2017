38f4dc61be57b1d8fb87f8dde3b6ca41512dfe29
Fede <Fede@Aula-B4-PC-32>
2017-02-03 11:42:06 +0000

    uh8hubugb

---
+++ herencia/src/herencia/persona.java
@@ -0,0 +1,24 @@
+package herencia;
+
+public class persona {
+  private String nombre;
+  private String Dni;
+  private int edad;	
+  	
+  public persona (String nombre, String dni, int edad) {
+      this.nombre = nombre;
+      this.Dni = Dni;
+      this.edad = edad();  
+      
+      public String getNombre () { 
+    	  return nombre;  
+    	  }
+      public String getDni() { 
+    	  return Dni;  
+    	  }
+      public int getEdad () {
+    	  return edad;   
+    	  
+
+}
+}
